686370895efdd28fa5397d124cea88b83979688b
Matriz <matriz@hotmail.it>
2026-01-21 20:51:20 +0100

    typo

--- app/src/hooks/useSearchMods.ts
+++ app/src/hooks/useSearchMods.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { useApiKey } from './useApiKey';
-import { curseforgeApi } from '../api/courseforge';
+import { curseforgeApi } from '../api/curseforge';
 import type { PaginatedResponse, Mod } from '../api/types';
 import { API } from '../config/constants';
 
